dfb472b701f37391df695f80dea5e9703deeeec7
shivam24121 <Shivam050905@gmail.com>
2025-12-15 21:03:07 +0530

    Create sol.sql

---
+++ SESSION 2/sol.sql
@@ -0,0 +1,15 @@
+-- G1_23BCS14207_SHIVAM
+
+SELECT A1.machine_id,AVG(A2.timestamp-A1.timestamp)
+AS processing_time
+FROM Activity A1
+LEFT JOIN 
+Activity A2 
+ON A1.machine_id=A2.machine_id
+AND 
+A1.process_id=A2.process_id
+AND 
+A2.activity_type='end'
+AND 
+A1.activity_type='start'
+GROUP BY A1.machine_id;
